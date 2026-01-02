721bb510f60f0e9a75d6a17fb5a6ab884a879819
anjalibehere05 <anjalibehere05@gmail.com>
2026-01-02 08:46:02 +0530

    final project

---
+++ final/Core/Inc/MQ2.h
@@ -0,0 +1,21 @@
+/*
+ * MQ2.h
+ *
+ *  Created on: Jan 1, 2026
+ *      Author: Acer
+ */
+
+#ifndef INC_MQ2_H_
+#define INC_MQ2_H_
+#include"main.h"
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdint.h>
+void SystemClock_Config(void);
+
+/* USER CODE BEGIN PFP */
+
+void smoke_moniter_task(void);
+
+#endif /* INC_MQ2_H_ */
+
